30dbcc166373010a76ec02ff61c6b8a6dadbda73
monolok <antoinebertin@me.com>
2017-03-12 10:28:36 -0500

    conversion

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,13 @@
 class PaymentsController < ApplicationController
+
   def index
-  	
+  	flash[:kraken] = HTTParty.get("https://api.kraken.com/0/public/Ticker?pair=XXBTZEUR")["result"]["XXBTZEUR"]["c"][0]
   end
+
+  def conversion
+  	flash[:value] = params["value"].to_f
+  	flash[:btc_to_get] = (flash[:value] / flash[:kraken].to_f).round(4)
+  	redirect_to root_path
+  end
+  
 end
